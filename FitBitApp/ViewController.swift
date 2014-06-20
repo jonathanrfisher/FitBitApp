689af4c6cdcd3c62ad70637413f93204d5b22292
Jonathan Fisher <jonathanfisher684@air-watch.com>
2014-06-20 09:53:07 -0400

    Initial Commit

---
+++ FitBitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitBitApp
+//
+//  Created by Jonathan Fisher on 6/20/14.
+//  Copyright (c) 2014 JonathanFisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
